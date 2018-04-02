18c60a931c7788943c9b3e3387b4515cfb57e7a3
Alexa Lomotan <alexa.lomotan@phantom.us>
2018-04-02 16:39:41 -0700

    Saving work before attempting update_dependencies script

---
+++ cylance_consts.py
@@ -0,0 +1,21 @@
+# --
+# File: cylance_consts.py
+#
+# Copyright (c) Phantom Cyber Corporation, 2018
+#
+# This unpublished material is proprietary to Phantom Cyber.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Phantom Cyber.
+#
+# --
+
+CYLANCE_JSON_USERNAME = "username"
+CYLANCE_JSON_PASSWORD = "password"
+CYLANCE_JSON_REGION_CODE = "region_code"
+
+CYLANCE_JSON_TENANT_ID = "tenant_id"
+CYLANCE_JSON_APPLICATION_ID = "application_id"
+CYLANCE_JSON_APPLICATION_SECRET = "application_secret"
