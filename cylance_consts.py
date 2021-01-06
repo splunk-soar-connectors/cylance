5eb2601b66791bcaadbb3dacfbb08051e07bf531
Nancy Parekh <nparekh@splunk.com>
2021-01-06 22:10:06 +0000

    PAPP-13067: 2to3 conversion and developer best practices

--- cylance_consts.py
+++ cylance_consts.py
@@ -1,5 +1,5 @@
 # File: cylance_consts.py
-# Copyright (c) 2019 Splunk Inc.
+# Copyright (c) 2018-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
@@ -13,6 +13,15 @@ CYLANCE_JSON_TENANT_ID = "tenant_id"
 CYLANCE_JSON_APPLICATION_ID = "application_id"
 CYLANCE_JSON_APPLICATION_SECRET = "application_secret"
 
+CYLANCE_REGION_CODES = {
+    "Asia-Pacific - North": "-apne1",
+    "Asia-Pacific - Southeast": "-au",
+    "Europe - Central": "-euc1",
+    "Government": ".us",
+    "North America": "",
+    "South America": "-sae1"
+}
+
 CYLANCE_AUTH_TOKEN_ERR = "Could not generate an auth token"
 CYLANCE_ACCESS_TOKEN_ERR = "Could not generate an access token"
 
@@ -21,6 +30,6 @@ CYLANCE_UNBLOCK_HASH_SUCC = "Successfully unblocked hash"
 CYLANCE_BLOCK_HASH_ALREADY_BLOCKED_SUCC = "Hash is already blocked"
 CYLANCE_UNBLOCK_HASH_ALREADY_UNBLOCKED_SUCC = "Hash is already unblocked"
 CYLANCE_UPDATE_ZONE_SUCC = "Successfully updated zone"
-CYLANCE_ERR_INVALID_PARAM = "Please provide non-zero positive integer in {param}"
+CYLANCE_ERR_INVALID_PARAM = "Please provide a non-zero positive integer in {param}"
 
 DEFAULT_MAX_RESULTS = 200
